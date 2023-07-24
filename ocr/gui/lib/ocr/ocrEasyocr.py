aac54f9f70bfaed1d4d57dfe789c1a84244b7257
siwons01 <siwons01@naver.com>
2023-07-25 08:20:48 +0900

    remove abstract

--- ocr/gui/lib/ocr/ocrEasyocr.py
+++ ocr/gui/lib/ocr/ocrEasyocr.py
@@ -1,12 +1,13 @@
+"""
 try :
     from lib.ocr.baseocr import baseocr
 except Exception as e:
     print(e)
     import baseocr
-    
+"""
 import easyocr
 
-class ocrEasyocr(baseocr):
+class ocrEasyocr():
     #원본 언어 설정 영어 인식
     ocr = easyocr.Reader(['en'])
     #cls 는 글자가 회전 되어 있을 때 인식 하는 것 설정
